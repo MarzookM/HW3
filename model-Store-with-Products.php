28111b45e31a0e1c8aa678a12ed27430e5127784
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 02:29:36 -0500

    Create model-Store-with-Products.php

---
+++ model-Store-with-Products.php
@@ -0,0 +1,18 @@
+<?php
+function selectStorewithProducts($cid){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT P.ProductID, ProductName, ProductPrice, StoreName, StoreAddress FROM `Product` P join Store s on s.ProductID = P.ProductID where s.ProductID=?"); 
+        $stmt->bind_param("i", $cid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+?>
