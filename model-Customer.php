3550fbe77a3a22a70d8f901e84592793431d52db
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 00:06:36 -0500

    Update model-Customer.php

--- model-Customer.php
+++ model-Customer.php
@@ -2,7 +2,7 @@
 function selectCustomer(){
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT CustomerID, CustomerName, CustomerAddress FROM 'Customer'"); //put code
+        $stmt = $conn->prepare("SELECT CustomerID, CustomerName, CustomerAddress FROM 'Customer'"); 
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
