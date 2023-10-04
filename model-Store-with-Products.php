92ac8c67222c1c6fac5b5ea8b231da1e252d44b1
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 02:42:30 -0500

    Update model-Store-with-Products.php

--- model-Store-with-Products.php
+++ model-Store-with-Products.php
@@ -1,9 +1,9 @@
 <?php
-function selectStorewithProducts($cid){
+function selectStorewithProducts($ccid){
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT P.ProductID, ProductName, ProductPrice, StoreName, StoreAddress FROM `Product` P join Store s on s.ProductID = P.ProductID where s.ProductID=?"); 
-        $stmt->bind_param("i", $cid);
+        $stmt->bind_param("i", $ccid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
