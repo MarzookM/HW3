ba91bc856ed068c5fa24025196114df1e544e299
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 01:48:40 -0500

    Update model-Customers-with-products.php

--- model-Customers-with-products.php
+++ model-Customers-with-products.php
@@ -3,7 +3,7 @@ function selectCustomerswithproducts($iid){
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT P.ProductID, ProductName, ProductPrice, StoreName, StoreAddress FROM `Product` P join Store s on s.ProductID = P.ProductID where s.CustomerID=?"); 
-        $stmt->blind_param("i", $iid);
+        $stmt->bind_param("i", $iid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
