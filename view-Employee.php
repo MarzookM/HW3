e13d7880e230c41a79cc2005d85119aaf83e56a7
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 03:53:04 -0500

    Update view-Employee.php

--- view-Employee.php
+++ view-Employee.php
@@ -1,30 +1,29 @@
 <h1>Employee</h1>
-<div class ="card-group">
-<?php
-while($Customers = $Customer->fetch_assoc()) {
-  ?>
-  <div class = "card">
-    <div class="card-body">
-    <h5 class ="card-title"><?php echo Customers['CustomerName']; ?></h5>
-    <p class ="card-text">
-    <ul class="list-group">
-  <?php
-    $Product = selectCustomerswithproducts($Employees[EmployeeName]);
-    while($Product = $Product->fetch_assoc()){
-      ?>
-        <td><?php echo $Products['ProductID']; ?></td>
-        <td><?php echo $Products['ProductName']; ?></td>  
-        <td><?php echo $Products['ProductPrice']; ?></td>
-      <?php
-    }
-  ?>
-    </ul>
-    </p>
-      <p class="card-text"><small class="text-body-secondary">Office: <?php echo $Customers['CustomerID']; ?></small></p>
-      
-    </div>
-  </div>
-  <?php
-}
+<div class = "table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Title</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php                 //from control page
+while($Employees = $Employee ->fetch_assoc())
+  {
 ?>
-  
+<tr> 
+  <td><?php echo $Employees['EmployeeID']; ?></td>
+  <td><?php echo $Employees['EmployeeName']; ?></td>  
+  <td><?php echo $Employees['EmployeeTitle']; ?></td>
+<?php
+    
+  }
+?>
+      
+    </tbody>
+
+
+      
+    </thead>
