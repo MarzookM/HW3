f7bc917e9bbfd586f199b1e079d3da70c8cac775
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 01:26:19 -0500

    Create model-Product.php

---
+++ model-Product.php
@@ -0,0 +1,17 @@
+<?php
+function selectProduct(){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT `ProductID`, `ProductName`, `ProductPrice` FROM `Product`  "); 
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+?>
