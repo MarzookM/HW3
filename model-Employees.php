1347628d96d35875efa0611d4e03c27ce7b9bf7e
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 03:24:02 -0500

    Create model-Employees.php

---
+++ model-Employees.php
@@ -0,0 +1,17 @@
+<?php
+function selectCustomer(){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT `EmployeeID`, `EmployeeName`, `EmployeeTitle` FROM `Employee`  "); 
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+?>
