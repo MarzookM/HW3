9c3a8ad90cce6f60d84f090d22349263c11bac81
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 01:04:51 -0500

    Create Customer-with-products.php

---
+++ Customer-with-products.php
@@ -0,0 +1,9 @@
+<?php
+require_once("util-db.php");
+require_once("model-Customers-with-products.php"); // going to create 
+$pageTitle = "Customer with Products";
+include "view-header.php";
+$Product = selectCustomerswithproducts();
+include "view-Customers-with-products.php"; //going to create
+include "view-footer.php";
+?> 
