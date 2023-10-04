62846ab179ef9e5d7fa4d2d6259e3748613048ec
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 01:14:42 -0500

    Create model-Customers-with-products.php

---
+++ model-Customers-with-products.php
@@ -0,0 +1,18 @@
+<?php
+function selectCustomerswithproducts($iid){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT P.ProductID, ProductName, ProductPrice, StoreName, StoreAddress FROM `Product` P join Store s on s.ProductID = P.ProductID where s.CustomerID=1"); 
+        $stmt->blind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+?>
