f0695e4058d4d3ca0e30a94cb1d1148cbdd8c70a
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 02:58:38 -0500

    Update view-Product.php

--- view-Product.php
+++ view-Product.php
@@ -20,8 +20,8 @@ while($Products = $Product ->fetch_assoc())
   <td><?php echo $Products['ProductPrice']; ?></td>
   <td>
     <form method ="post" action="Store-with-Products.php">
-      <input type = "hidden" name ='cid' value="<?php echo $Products['ProductID']; ?>">
-      <button type="submit" class="btn btn-primary">Submit</button>
+          <input type = "hidden" name ='cid' value="<?php echo $Products['ProductID']; ?>">
+          <button type="submit" class="btn btn-primary">Products</button>
     </form>
   </td>
 
