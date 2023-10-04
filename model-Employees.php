3862265920724e67a1292fda92d6935cc42360ac
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 04:00:35 -0500

    Update model-Employees.php

--- model-Employees.php
+++ model-Employees.php
@@ -12,19 +12,7 @@ function selectEmployee(){
         throw $e;
     }
 }
-function selectCustomer(){
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT `CustomerID`, `CustomerName`, `CustomerAddress` FROM `Customer` "); 
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
+
 
 
 ?>
