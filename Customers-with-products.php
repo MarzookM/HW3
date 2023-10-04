cad5d8174a03aa3af09c4d6b6acf372444ec462b
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 02:05:52 -0500

    Update Customers-with-products.php

--- Customers-with-products.php
+++ Customers-with-products.php
@@ -3,7 +3,7 @@ require_once("util-db.php");
 require_once("model-Customers-with-products.php"); // going to create 
 $pageTitle = "Customer with Products";
 include "view-header.php";
-$Product = selectCustomerswithproducts($_GET['ID']);
+$Product = selectCustomerswithproducts();
 include "view-Customers-with-products.php"; //going to create
 include "view-footer.php";
 ?> 
