e109eb7b2f0e4bd85389bc8496b25bb7fb46ffc1
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 03:27:58 -0500

    Update model-Employees.php

--- model-Employees.php
+++ model-Employees.php
@@ -13,5 +13,19 @@ function selectCustomer(){
     }
 }
 
+function selectCustomerswithproducts($iid){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT P.ProductID, ProductName, ProductPrice, StoreName, StoreAddress FROM `Product` P join Store s on s.ProductID = P.ProductID where s.CustomerID=?"); 
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
 ?>
