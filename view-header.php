bc0dd3d3874c16690d95ab022b77eccd4356b65c
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 02:26:54 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -27,7 +27,7 @@
           <a class="nav-link" href="Product.php">Product</a>
         </li>
            <li class="nav-item">
-          <a class="nav-link" href="Customers-with-products.php">Customers with products</a>
+          <a class="nav-link" href="Stores-and-products.php">Stores and products</a>
         </li>  
       </ul>
     </div>
