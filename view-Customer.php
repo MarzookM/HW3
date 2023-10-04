28d05662adc306cf17711e0c75a15f8dc75c3b87
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 01:00:16 -0500

    Update view-Customer.php

--- view-Customer.php
+++ view-Customer.php
@@ -17,6 +17,7 @@ while($Customers = $Customer ->fetch_assoc())
   <td><?php echo $Customers['CustomerID']; ?></td>
   <td><?php echo $Customers['CustomerName']; ?></td>  
   <td><?php echo $Customers['CustomerAddress']; ?></td>
+  <td><a href = "Customers-with-products.php?id=<?php echo $Customer['CustomerID']; ?>">Customer</a></td>
 </tr>
 <?php
     
