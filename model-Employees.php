335f547fc850477823974ab48be6b3eed1c57c92
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-04 03:50:46 -0500

    Update model-Employees.php

--- model-Employees.php
+++ model-Employees.php
@@ -2,7 +2,7 @@
 function selectCustomer(){
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT `CustomerID`, `CustomerName`, `CustomerAddress` FROM `Customer` "); 
+        $stmt = $conn->prepare(SELECT `EmployeeID`, `EmployeeName`, `EmployeeTitle` FROM `Employee` ); 
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -13,20 +13,5 @@ function selectCustomer(){
     }
 }
 
-
-function selectCustomerswithproducts($iid){
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT P.ProductID, ProductName, ProductPrice, StoreName, StoreAddress FROM `Product` P join Store s on s.ProductID = P.ProductID where s.CustomerID=?"); 
-        $stmt->bind_param("i", $iid);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
 
 ?>
