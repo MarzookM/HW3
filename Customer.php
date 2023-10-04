15c2c0c668409950d22c97d1c3ae72272e44accc
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-03 23:29:50 -0500

    Update Customer.php

--- Customer.php
+++ Customer.php
@@ -3,7 +3,7 @@ require_once("util-db.php");
 require_once("model-Customer.php"); // going to create 
 $pageTitle = "Customer";
 include "view-header.php";
-$customer = selectCustomer();
+$Customer = selectCustomer();
 include "view-Customer.php"; //going to create
 include "view-footer.php";
 ?> 
