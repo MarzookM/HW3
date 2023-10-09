1c0fe23c8551586d8357d971c1917ea62e6a842f
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-09 13:42:02 -0500

    Update Store-with-Products.php

--- Store-with-Products.php
+++ Store-with-Products.php
@@ -3,7 +3,7 @@ require_once("util-db.php");
 require_once("model-Store-with-Products.php"); // going to create 
 $pageTitle = "Store with Products";
 include "view-header.php";
-$Store = selectStore($_GET['cid']);
+$Store = selectStore($_POST['cid']);
 include "view-Store-with-Products.php"; //going to create
 include "view-footer.php";
 ?> 
