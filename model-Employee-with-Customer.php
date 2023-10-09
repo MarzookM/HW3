7608a40570bbdc11e533a181fedc7dcc1cf20365
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-09 14:30:06 -0500

    Update and rename model-Store-With-Employees.php to model-Employee-with-Customer.php

---
+++ model-Employee-with-Customer.php
@@ -0,0 +1,31 @@
+<?php
+function selectCustomer(){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT `CustomerID`, `CustomerName`, `CustomerAddress` FROM `Customer` "); 
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectCustomerswithproducts($iid){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT P.ProductID, ProductName, ProductPrice, StoreName, StoreAddress FROM `Product` P join Store s on s.ProductID = P.ProductID where s.CustomerID=?"); 
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
