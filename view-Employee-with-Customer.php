0ae67ea1e9a394c1219701a9fcadc0d4d349f3c8
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-24 17:08:44 -0500

    Update view-Employee-with-Customer.php

--- view-Employee-with-Customer.php
+++ view-Employee-with-Customer.php
@@ -1,32 +1,37 @@
-<h1>Employees and Customer</h1>
-<div class="card-group">
-<?php             
-while($Customers = $Customer ->fetch_assoc())
-  {
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Employees and Customers</title>
+</head>
+<body>
+  <h1>Employees and Customers</h1>
+  <div class="card-group">
+    <?php             
+    while ($Customers = $Customer->fetch_assoc()) {
+    ?>
     <div class="card">
       <div class="card-body">
         <h5 class="card-title"><?php echo $Customers['CustomerName']; ?></h5>
-        <p class = "card-text">
-        <ul class="list-group">
-<?php
-  $Products = selectCustomerswithproducts($Customer['CustomerID']);
-  while($Product = $Products->fetch_assoc()){
-?>
-    <li class="list-group-items"><?php echo $Products['ProductName']; ?> - <?php echo $Products['ProductPrice']; ?> - <?php echo $Products['ProductID']; ?> - </li>
-  <td><?php echo $Products['ProductID']; ?></td>
-  <td><?php echo $Products['ProductName']; ?></td>  
-  <td><?php echo $Products['ProductPrice']; ?></td>
-  <?php
- 
-  }
-?>
-      </ul>
-      </p>
-      <p class="card-text"><small class="text-body-secondary">Address: <?php echo $Customers['CustomerAddress']; ?></small></p>
+        <p class="card-text">
+          <ul class="list-group">
+            <?php
+            $Products = selectCustomerswithproducts($Customers['CustomerID']);
+            while ($Product = $Products->fetch_assoc()) {
+            ?>
+            <li class="list-group-item">
+              <?php echo $Product['ProductName']; ?> - <?php echo $Product['ProductPrice']; ?> - <?php echo $Product['ProductID']; ?>
+            </li>
+            <?php
+            }
+            ?>
+          </ul>
+        </p>
+        <p class="card-text"><small class="text-muted">Address: <?php echo $Customers['CustomerAddress']; ?></small></p>
       </div>
     </div>
     <?php
-  }
-?>
-</div>
+    }
+    ?>
+  </div>
+</body>
+</html>
