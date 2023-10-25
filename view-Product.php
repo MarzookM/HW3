0da9d4f47f700f320e17b67d795963b7d46c91a2
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-24 23:00:11 -0500

    Update view-Product.php

--- view-Product.php
+++ view-Product.php
@@ -30,6 +30,9 @@ while($Products = $Product ->fetch_assoc())
   <td><?php echo $Products['ProductName']; ?></td>  
   <td><?php echo $Products['ProductPrice']; ?></td>
   <td>
+          <?php
+include "view-Product-editform.php"; 
+?>
   
   </td>
   <td>
