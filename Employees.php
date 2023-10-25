ad61187242912a7d3d4062d25a47381d23447f62
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-25 10:51:07 -0500

    Update Employees.php

--- Employees.php
+++ Employees.php
@@ -1,36 +1,38 @@
 <?php
 require_once("util-db.php");
-require_once("model-Employees.php"); // going to create 
+require_once("model-Employees.php");
+
 $pageTitle = "Employees";
+
 include "view-header.php";
 
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if(insertEmployee($_POST['cNumber'], $_POST['cDesc'])){
-        echo '<div class="alert alert-success" role="alert">Product Added.</div>';
-      }else{
+      if (insertEmployee($_POST['employeeName'], $_POST['employeeDescription'])) {
+        echo '<div class="alert alert-success" role="alert">Employee Added.</div>';
+      } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
-      if(updateEmployee($_POST['cDesc'], $_POST['cid'])){
-        echo '<div class="alert alert-success" role="alert">Product edited.</div>';
-      }else{
+      if (updateEmployee($_POST['employeeDescription'], $_POST['employeeId'])) {
+        echo '<div class="alert alert-success" role="alert">Employee edited.</div>';
+      } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Delete":
-     if(deleteEmployee($_POST['cid'])){
-        echo '<div class="alert alert-success" role="alert">Course Deleted.</div>';
-      }else{
+      if (deleteEmployee($_POST['employeeId'])) {
+        echo '<div class="alert alert-success" role="alert">Employee Deleted.</div>';
+      } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
-    
   }
 }
-$Employee = selectEmployee();
-include "view-Employee.php"; //going to create
+
+$Employees = selectEmployee();
+include "view-Employees.php";
 include "view-footer.php";
-?> 
+?>
