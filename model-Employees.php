4b7f07dc57dc679b315234aec0cb668ebcdbb002
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-25 02:55:58 -0500

    Update model-Employees.php

--- model-Employees.php
+++ model-Employees.php
@@ -12,7 +12,45 @@ function selectEmployee(){
         throw $e;
     }
 }
-
+function insertEmployee($cNumber, $cDesc){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Employee` (`EmployeeTitle`, `EmployeeName`) VALUES (?,?)"); 
+        $stmt->bind_param("ss", $cNumber, $cDesc);
+        $success = $stmt->execute();
+        $conn->close();
+        return  $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateEmployee($cNumber, $cDesc, $cid){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `Employee` set `EmployeeTitle` = ?, `EmployeeName` =? where `EmployeeID` = ?" ); 
+        $stmt->bind_param("ssi", $cNumber, $cDesc, $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteEmployee($cid){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from Employee where EmployeeID =?"); 
+        $stmt->bind_param("i", $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
 
 ?>
