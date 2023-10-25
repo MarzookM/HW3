e4b98b19d3050965f29d7213201a2741cc76f87f
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-24 20:45:06 -0500

    Update model-Product.php

--- model-Product.php
+++ model-Product.php
@@ -12,6 +12,44 @@ function selectProduct(){
         throw $e;
     }
 }
-
+function insertProduct($cNumber, $cDesc){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Product` (`ProductPrice`, `ProductName`) VALUES (?,?)"); 
+        $stmt->bind_param("ss", $cNumber, $cDesc);
+        $success = $stmt->execute();
+        $conn->close();
+        return  $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateProduct($cNumber, $cDesc, $cid){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `Product` set `ProductPrice` = ?, `ProductName` =? where `ProductID` = ?" ); 
+        $stmt->bind_param("ssi", $cNumber, $cDesc, $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteProduct($cid){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from course where ProductID =?"); 
+        $stmt->bind_param("i", $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
 ?>
