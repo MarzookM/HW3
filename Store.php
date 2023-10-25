f9495cc9d674c00891ffedfc31eaa9192c32e1be
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-25 11:51:39 -0500

    Create Store.php

---
+++ Store.php
@@ -0,0 +1,37 @@
+<?php
+require_once("util-db.php");
+require_once("model-Store.php");
+$pageTitle = "Customer";
+include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if(insertStore($_POST['cNumber'], $_POST['cDesc'])){
+        echo '<div class="alert alert-success" role="alert">Store Added.</div>';
+      }else{
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if(updateStore($_POST['cDesc'], $_POST['cid'])){
+        echo '<div class="alert alert-success" role="alert">Store edited.</div>';
+      }else{
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+     if(deleteStore($_POST['cid'])){
+        echo '<div class="alert alert-success" role="alert">Course Deleted.</div>';
+      }else{
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    
+  }
+}
+
+$Store = selectStore();
+include "view-Store.php";
+include "view-footer.php";
+?>
