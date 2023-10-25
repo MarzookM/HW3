08b7372b1ec8636870dd558e17999c559a6382e5
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-25 10:46:20 -0500

    Update view-Employee-with-Customer-editform.php

--- view-Employee-with-Customer-editform.php
+++ view-Employee-with-Customer-editform.php
@@ -1,5 +1,5 @@
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editStoreModal<?php echo $Customers['StoreID']; ?>">
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editStoreModal<?php echo $Customer['StoreID']; ?>">
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editCustomerModal<?php echo $Customers['CustomerID']; ?>">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -7,50 +7,46 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="#editStoreModal<?php echo $Customers['StoreID']; ?>" tabindex="-1" aria-labelledby="#editStoreModal<?php echo $Customers['StoreID']; ?>" aria-hidden="true">
-<div class="modal fade" id="#editStoreModal<?php echo $Customer['StoreID']; ?>" tabindex="-1" aria-labelledby="#editStoreModal<?php echo $Customer['StoreID']; ?>" aria-hidden="true">
+<div class="modal fade" id="editCustomerModal<?php echo $Customers['CustomerID']; ?>" tabindex="-1" aria-labelledby="editCustomerModalLabel<?php echo $Customers['CustomerID']; ?>" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="#editStoreModal<?php echo $Customers['StoreID']; ?>">Edit Store</h1>
-                <h1 class="modal-title fs-5" id="#editStoreModal<?php echo $Customer['StoreID']; ?>">Edit Store</h1>
+                <h1 class="modal-title fs-5" id="editCourseModalLabel<?php echo $Customers['CustomerID']; ?>">Edit Doctor With Patients</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form method="post" action="">
                     <div class="mb-3">
-                        <label for="iid<?php echo $Customers['StoreID'];?>" class="form-label">CustomerID</label>
-                        <input type="text" class="form-control" id="iid<?php echo $Customers['StoreID']; ?>" name="iid" value="<?php echo $Customers['StoreID']; ?>">
-                        <label for="iid<?php echo $Customer['StoreID'];?>" class="form-label">CustomerID</label>
-                        <input type="text" class="form-control" id="iid<?php echo $Customer['StoreID']; ?>" name="iid" value="<?php echo $Customer['StoreID']; ?>">
+                        <label for="iid<?php echo $Customers['CustomerID']; ?>" class="form-label">CustomerID</label>
+                        <input type="text" class="form-control" id="iid<?php echo $Customers['CustomerID']; ?>" name="iid" value="<?php echo $Customers['CustomerID']; ?>">
                     </div>
                     <div class="mb-3">
-                        <label for="cid<?php echo $Customers['StoreID']; ?>" class="form-label">ProductID</label>
-                        <input type="text" class="form-control" id="cid<?php echo $Customers['StoreID'];  ?>" name="cid" value="<?php echo $Customers['StoreID'];  ?>">
-                        <label for="cid<?php echo $Customer['StoreID']; ?>" class="form-label">ProductID</label>
-                        <input type="text" class="form-control" id="cid<?php echo $Customer['StoreID'];  ?>" name="cid" value="<?php echo $Customer['StoreID'];  ?>">
+                        <label for="cid<?php echo $Customers['CustomerID']; ?>" class="form-label">ProductID</label>
+                        <input type="text" class="form-control" id="cid<?php echo $Customers['CustomerID']; ?>" name="cid" value="<?php echo $Customers['ProductID']; ?>">
                     </div>
                     <div class="mb-3">
-                        <label for="Cname<?php echo $Customer['StoreID']; ?>" class="form-label">CustomerName</label>
-                        <input type="text" class="form-control" id="Cname<?php echo $Customer['StoreID'];  ?>" name="Cname" value="<?php echo $Customer['StoreID'];  ?>">
+                        <label for="Cname<?php echo $Customers['CustomerID']; ?>" class="form-label">CustomerName</label>
+                        <input type="text" class="form-control" id="Cname<?php echo $Customers['CustomerID']; ?>" name="Cname" value="<?php echo $Customers['CustomerName']; ?>">
                     </div>
                     <div class="mb-3">
-                        <label for="PName<?php echo $Customers['StoreID'];  ?>" class="form-label">ProductName</label>
-                        <input type="text" class="form-control" id="PName<?php echo $Customers['StoreID']; ?>" name="PName" value="<?php echo $Customers['StoreID'];?>">
-                        <label for="PName<?php echo $Customer['StoreID'];  ?>" class="form-label">ProductName</label>
-                        <input type="text" class="form-control" id="PName<?php echo $Customer['StoreID']; ?>" name="PName" value="<?php echo $Customer['StoreID'];?>">
+                        <label for="PPrice<?php echo $Customers['CustomerID']; ?>" class="form-label">ProductPrice</label>
+                        <input type="text" class="form-control" id="PPrice<?php echo $Customers['CustomerID']; ?>" name="PPrice" value="<?php echo $Customers['ProductName']; ?>">
                     </div>
                     <div class="mb-3">
-                        <label for="iid<?php echo $Customers['StoreID']; ?>" class="form-label">Customer</label>
-                        <label for="iid<?php echo $Customer['StoreID']; ?>" class="form-label">Customer</label>
-                        <?php $CustomerList = selectCustomerForInput();
-                        $selectedCustomer = $Customer['StoreID'];
-                        include "view-Customer-input-list.php"; ?>
+                        <label for="iid<?php echo $Customers['CustomerID']; ?>" class="form-label">Customer</label>
+                        <?php
+                        $CustomerList = selectCustomerForInput();
+                        $selectedCustoemr = $Products['ProductID'];
+                        include "view-Customer-input-list.php";
+                        ?>
                     </div>
-                    <input type="hidden" name="iid" value="<?php echo $Customers['StoreID'];?>">
-                    <input type="hidden" name="cid" value="<?php echo $Customers['CustomerID']; ?>">
-                    <input type="hidden" name="iid" value="<?php echo $Customer['StoreID'];?>">
-                    <input type="hidden" name="cid" value="<?php echo $Customer['CustomerID']; ?>">
+                    <input type="hidden" name="iid" value="<?php echo $Customers['CustomerID']; ?>">
+                    <input type="hidden" name="tid" value="<?php echo $Customers['CustomerID']; ?>">
                     <input type="hidden" name="actionType" value="Edit">
                     <button type="submit" class="btn btn-primary">Save</button>
                 </form>
+            </div>
+        </div>
+    </div>
+</div>
+
