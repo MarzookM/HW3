873d0dd1b6cc1ccb9d52b3bc43c12a0c48b64f3f
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-25 11:06:06 -0500

    Update model-Employees.php

--- model-Employees.php
+++ model-Employees.php
@@ -25,11 +25,11 @@ function insertEmployee($cNumber, $cDesc){
         throw $e;
     }
 }
-function updateEmployee($cNumber, $cDesc){
+function updateEmployee($cid,$cNumber, $cDesc){
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("update `Employee` set `EmployeeTitle` = ?, `EmployeeName` =? where `EmployeeID` = ?" ); 
-        $stmt->bind_param("ssi", $cNumber, $cDesc);
+        $stmt->bind_param("cid", $cNumber, $cDesc);
         $success = $stmt->execute();
         $conn->close();
         return $success;
