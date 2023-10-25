a0f76dd92406aa131be106352b33d595605596dc
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-25 01:35:49 -0500

    Update model-Employee-with-Customer.php

--- model-Employee-with-Customer.php
+++ model-Employee-with-Customer.php
@@ -28,4 +28,50 @@ function selectCustomerswithproducts($iid){
     }
 }
 
+
+function insertStore($iid, $cid, $Cname, $Pname, $PPrice) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Store` (`CustomerID`, `ProductID`, `CustomerName`, `ProductName`, `ProductPrice`) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("iisss",$cid, $Cname, $Pname, $PPrice);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateStore($iid, $cid, $Cname, $Pname, $PPrice, $sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `Store` SET `ProductName` = ?, `CustomerName` = ?, `ProductPrice` = ?, `CustomerID` = ? WHERE ProductID = ?");
+        $stmt->bind_param("iisssi",$iid, $cid, $Cname, $Pname, $PPrice, $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deleteStore($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM Store WHERE StoreID = ?");
+        $stmt->bind_param("i", $sid);
+        $stmt->execute();
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
 ?>
