6abd9a5a4cc2df76958b54c4c1df53b408b28203
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-24 21:17:25 -0500

    Update Product.php

--- Product.php
+++ Product.php
@@ -3,6 +3,14 @@ require_once("util-db.php");
 require_once("model-Product.php"); // going to create 
 $pageTitle = "Customer";
 include "view-header.php";
+
+if(isset($_POST['actionType'])){
+  switch($_POST['actionType'])){
+    case "Add":
+      insertProduct($_POST['cNumber'], $_POST['cDesc']);
+      break;
+  }
+}
 $Product = selectProduct();
 include "view-Product.php"; //going to create
 include "view-footer.php";
