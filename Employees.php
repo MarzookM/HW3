c68e148547a8dd05a6fa0cfb0429dfe65fea90ed
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-25 11:00:13 -0500

    Update Employees.php

--- Employees.php
+++ Employees.php
@@ -3,26 +3,25 @@ require_once("util-db.php");
 require_once("model-Employees.php"); // going to create 
 $pageTitle = "Employees";
 include "view-header.php";
+
+
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if(insertProduct($_POST['cNumber'], $_POST['cDesc'])){
       if(insertEmployee($_POST['cNumber'], $_POST['cDesc'])){
-        echo '<div class="alert alert-success" role="alert">Product Added.</div>';
+        echo '<div class="alert alert-success" role="alert">Employee Added.</div>';
       }else{
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
-      if(updateProduct($_POST['cDesc'], $_POST['cid'])){
       if(updateEmployee($_POST['cDesc'], $_POST['cid'])){
-        echo '<div class="alert alert-success" role="alert">Product edited.</div>';
+        echo '<div class="alert alert-success" role="alert">Employee edited.</div>';
       }else{
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Delete":
-     if(deleteProduct($_POST['cid'])){
      if(deleteEmployee($_POST['cid'])){
         echo '<div class="alert alert-success" role="alert">Course Deleted.</div>';
       }else{
