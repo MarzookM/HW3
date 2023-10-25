43b124727dfce45a136bff09f769693fa2b19a93
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-24 21:40:19 -0500

    Update Product.php

--- Product.php
+++ Product.php
@@ -7,7 +7,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      insertProduct($_POST['cNumber'], $_POST['cDesc']);
+      if(insertProduct($_POST['cNumber'], $_POST['cDesc'])){
+        echo '<div class="alert alert-success" role="alert">Product Added.</div>"';
+      }else{
+        echo '<div class="alert alert-danger" role="alert">Error.</div>"';
+      }
       break;
   }
 }
