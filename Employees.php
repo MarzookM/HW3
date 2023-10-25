edab07bc78d323d6d591e2167eb317c2f462b0b9
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-25 10:55:23 -0500

    Update Employees.php

--- Employees.php
+++ Employees.php
@@ -16,7 +16,7 @@ if (isset($_POST['actionType'])) {
       }
       break;
     case "Edit":
-      if (updateEmployee($_POST['EmployeeTitle'], $_POST['EmployeeID'])) {
+      if (updateEmployee($_POST['EmployeeName'], $_POST['EmployeeTitle'])) {
         echo '<div class="alert alert-success" role="alert">Employee edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
