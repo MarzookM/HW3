5cac9b705f0fbcfa078382221197d89a7de44bc0
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-10-24 22:57:09 -0500

    Update Product.php

--- Product.php
+++ Product.php
@@ -19,14 +19,7 @@ if (isset($_POST['actionType'])) {
       }else{
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
-    break;
-    case "Edit":
-      if(updateProduct($_POST['cDesc'], $_POST['cid'])){
-        echo '<div class="alert alert-success" role="alert">Product edited.</div>';
-      }else{
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
+  
     
   }
 }
